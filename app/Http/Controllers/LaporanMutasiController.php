1ada59c03042c48748433828e1d5947e733f958c
Anang Firmansyah <anangfirmansyahp5@gmail.com>
2025-08-20 14:34:46 +0800

    test

---
+++ app/Http/Controllers/LaporanMutasiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaporanMutasiController extends Controller
+{
+    public function generateLaporan(Request $request)
+    {
+        dd($request);
+    }
+}
